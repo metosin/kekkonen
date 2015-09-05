ffaacc2e6748ad4fb9a2ba3b49d7a454e5b7b441
Tommi Reiman <tommi@iki.fi>
2015-09-05 21:58:40 +0300

    cleanup

--- src/kekkonen/ring.clj
+++ src/kekkonen/ring.clj
@@ -7,10 +7,6 @@
             [kekkonen.common :as kc]
             [clojure.string :as str]))
 
-;;
-;; options
-;;
-
 (def default-options
   {:types {:handler {:methods #{:post}}}
    :coercion {:query-params rsc/query-schema-coercion-matcher
@@ -18,21 +14,6 @@
               :form-params rsc/query-schema-coercion-matcher
               :header-params rsc/query-schema-coercion-matcher
               :body-params rsc/json-schema-coercion-matcher}})
-
-(def http-types {:get {:methods #{:get}}
-                 :head {:methods #{:head}}
-                 :patch {:methods #{:patch}}
-                 :delete {:methods #{:delete}}
-                 :options {:methods #{:options}}
-                 :post {:methods #{:post}}
-                 :put {:methods #{:put}}
-                 :any {:methods #{:get :head :patch :delete :options :post :put}}})
-
-(def http-type-resolver (k/type-resolver :get :head :patch :delete :options :post :put :any))
-
-;;
-;; beef
-;;
 
 (s/defn uri->action :- s/Keyword
   "Converts an action keyword from a uri string."
@@ -88,3 +69,18 @@
                           (assoc response :body coerced))
                         response))
                     response))))))))))
+
+;;
+;; http-types
+;;
+
+(def http-types {:get {:methods #{:get}}
+                 :head {:methods #{:head}}
+                 :patch {:methods #{:patch}}
+                 :delete {:methods #{:delete}}
+                 :options {:methods #{:options}}
+                 :post {:methods #{:post}}
+                 :put {:methods #{:put}}
+                 :any {:methods #{:get :head :patch :delete :options :post :put}}})
+
+(def http-type-resolver (k/type-resolver :get :head :patch :delete :options :post :put :any))
