38f4ed4908843812357eb9ec1371b38bfa5d0d85
Tommi Reiman <tommi@iki.fi>
2015-09-09 11:56:32 +0300

    hide web-details from the cars-api with wrapping ring-http-response statuses & predicates as cqrs-terms

--- src/kekkonen/cqrs.clj
+++ src/kekkonen/cqrs.clj
@@ -2,7 +2,9 @@
   (:require [kekkonen.core :as k]
             [kekkonen.api :as ka]
             [kekkonen.common :as kc]
-            [ring.util.http-response :as hr]))
+            [ring.util.http-response :as hr]
+            [ring.util.http-status :as hs]
+            [ring.util.http-predicates :as hp]))
 
 (def +cqrs-types+ {:query {:methods #{:get}
                            :parameters [[[:request :query-params] [:data]]]}
@@ -18,6 +20,19 @@
        :ring {:types +cqrs-types+}}
       options)))
 
+;;
+;; Return types
+;;
+
 (def success hr/ok)
 (def failure hr/bad-request)
 (def error hr/internal-server-error)
+
+(def success-status hs/ok)
+(def failure-status hs/bad-request)
+(def error-status hs/internal-server-error)
+
+(def success? hp/ok?)
+(def failure? hp/bad-request?)
+(def error? hp/internal-server-error?)
+
