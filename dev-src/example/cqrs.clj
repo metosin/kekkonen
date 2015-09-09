a0307515de210044cf62610c245488d6263ea2b3
Tommi Reiman <tommi@iki.fi>
2015-09-09 11:56:40 +0300

    more complete example

--- dev-src/example/cqrs.clj
+++ dev-src/example/cqrs.clj
@@ -4,37 +4,61 @@
             [plumbing.core :as p]
             [schema.core :as s]))
 
-(p/defnk ^:query get-items :- #{s/Str}
-  "Retrieves all"
+;;
+;; Schemas
+;;
+
+(s/defschema Item
+  "A database item"
+  {:name s/Str
+   :size (s/enum :S :M :L)
+   (s/optional-key :description) s/Str
+   :origin {:country (s/enum :FI :PO)}})
+
+;;
+;; Commands & Queries
+;;
+
+(p/defnk ^:query get-items
+  "Retrieves all items"
+  {:responses {success-status {:schema [Item]}}}
   [[:components db]]
-  (success @db))
+  (success (vals @db)))
 
-(p/defnk ^:command add-item! :- #{s/Str}
+(p/defnk ^:command add-item!
   "Adds an item to database"
-  [[:data item :- String]
-   [:components db]]
-  (success (swap! db conj item)))
+  {:responses {success-status {:schema Item}}}
+  [[:components db ids]
+   data :- Item]
+  (let [id (swap! ids (comp keyword inc))]
+    (success
+      (get (swap! db assoc id data) id))))
 
-(p/defnk ^:command reset-items! :- #{s/Str}
+(p/defnk ^:command reset-items!
   "Resets the database"
   [[:components db]]
   (success (swap! db empty)))
 
+(p/defnk ^:query ping [] (success {:ping "pong"}))
+(p/defnk ^:query pong [] (success {:pong "ping"}))
+
+;;
+;; Application
+;;
+
 (def app
   (cqrs-api
-    {:core {:handlers {:api 'example.cqrs}
-            :context {:components {:db (atom #{})}}}}))
+    {:info {:info {:title "Kekkonen"}}
+     :core {:handlers {:api {:item [#'get-items #'add-item! #'reset-items!]
+                             :system [#'ping #'pong]}}
+            :context {:components {:db (atom {})
+                                   :ids (atom 0)}}}}))
 
 (defn start []
-  (def server
-    (jetty/run-jetty
-      #'app
-      {:port 3000
-       :join? false})))
-
-(defn stop []
-  (.stop server))
+  (jetty/run-jetty
+    #'app
+    {:port 3000
+     :join? false}))
 
 (comment
-  (start)
-  (stop))
+  (start))
