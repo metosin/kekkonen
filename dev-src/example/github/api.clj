9ce712facb70ef3f5ce791ccb1ab78fa824f921b
Tommi Reiman <tommi@iki.fi>
2015-09-11 12:28:11 +0300

    github api

--- dev-src/example/github/api.clj
+++ dev-src/example/github/api.clj
@@ -17,6 +17,13 @@
    :watchers #{s/Str}
    :stargazers #{s/Str}})
 
+(s/def compojure-api
+  {:id 1
+   :name "Compojure API"
+   :allowed #{"seppo" "sirpa"}
+   :watchers #{}
+   :stargazers #{}})
+
 ;;
 ;; Commands & Queries
 ;;
@@ -31,22 +38,53 @@
   "Retrieves repo details"
   {:responses {success-status {:schema (s/maybe Repository)}}}
   [[:components repos]
-   [:data id :- s/Str]]
+   [:data id :- s/Int]]
   (success (@repos id)))
 
 (p/defnk ^:command fork
   "Forks a repo"
-  [[:data id :- s/Str]]
+  [[:data id :- s/Int]]
   (success {:forked id}))
 
-(p/defnk ^:query ping [] (success {:ping "pong"}))
-(p/defnk ^:query pong [] (success {:pong "ping"}))
+(p/defnk ^:command watch
+  "Watch a repo"
+  [user
+   [:data id :- s/Int]
+   [:components repos]]
+  (swap! repos #(update-in % [id :watchers] conj user))
+  (success {:forked id}))
 
+(p/defnk ^:command un-watch
+  "Watch a repo"
+  [user
+   [:data id :- s/Int]
+   [:components repos]]
+  (swap! repos #(update-in % [id :watchers] disj user))
+  (success {:forked id}))
+
+(p/defnk ^:command star
+  "Watch a repo"
+  [user
+   [:data id :- s/Int]
+   [:components repos]]
+  (swap! repos #(update-in % [id :stargazers] conj user))
+  (success))
+
+(p/defnk ^:command un-star
+  "Watch a repo"
+  [user
+   [:data id :- s/Int]
+   [:components repos]]
+  (swap! repos #(update-in % [id :stargazers] disj user))
+  (success))
 
 (do
   ;;
-  ;; calculators
+  ;; testsing
   ;;
+
+  (p/defnk ^:query ping [] (success {:ping "pong"}))
+  (p/defnk ^:query pong [] (success {:pong "ping"}))
 
   (p/defnk ^:query plus
     {:responses {success-status {:schema {:result s/Int}}}}
@@ -70,11 +108,18 @@
 (def app
   (cqrs-api
     {:info {:info {:title "Kekkonen"}}
-     :core {:handlers {:api {:github [#'get-repository #'list-repositorys #'fork]
+     :core {:handlers {:api {:github [#'get-repository
+                                      #'list-repositorys
+                                      #'fork
+                                      #'watch
+                                      #'un-watch
+                                      #'star
+                                      #'un-star]
                              :calculator [#'plus #'times #'inc!]
                              :security #'security/get-user
                              :system {:ping [#'ping #'pong]}}}
-            :context {:components {:repos (atom {})
+            :context {:components {:repos (atom {(:id compojure-api)
+                                                 compojure-api})
                                    :counter (atom 0)}}
             :user {:roles security/require-roles}}
      :ring {:transformers [security/api-key-authenticator]}}))
