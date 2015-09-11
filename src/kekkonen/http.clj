9133eede24f9994611832fe87129c83ba4dbe9d6
Tommi Reiman <tommi@iki.fi>
2015-09-11 11:03:19 +0300

    http-api cleaned up

--- src/kekkonen/http.clj
+++ src/kekkonen/http.clj
@@ -4,20 +4,16 @@
             [kekkonen.common :as kc]
             [schema.core :as s]))
 
-(def +http-types+ {:get {:methods #{:get}}
-                   :head {:methods #{:head}}
-                   :patch {:methods #{:patch}}
-                   :delete {:methods #{:delete}}
-                   :options {:methods #{:options}}
-                   :post {:methods #{:post}}
-                   :put {:methods #{:put}}
-                   :any {:methods #{:get :head :patch :delete :options :post :put}}})
-
-(def +http-type-resolver+ (k/type-resolver :get :head :patch :delete :options :post :put :any))
-
 (s/defn http-api [options]
   (ka/api
     (kc/deep-merge
-      {:core {:type-resolver +http-type-resolver+}
-       :ring {:types +http-types+}}
+      {:core {:type-resolver (k/type-resolver :get :head :patch :delete :options :post :put :any}
+       :ring {:types {:get {:methods #{:get}}
+                      :head {:methods #{:head}}
+                      :patch {:methods #{:patch}}
+                      :delete {:methods #{:delete}}
+                      :options {:methods #{:options}}
+                      :post {:methods #{:post}}
+                      :put {:methods #{:put}}
+                      :any {:methods #{:get :head :patch :delete :options :post :put}}}}}
       options)))
