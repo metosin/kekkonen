68d2122018e9cf92da1157e4d48d75fc3438f305
Tommi Reiman <tommi@iki.fi>
2015-09-18 08:28:09 +0300

    update dependencies, remove dependency badge (not on closers)

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
-  :dependencies [[prismatic/plumbing "0.4.4"]
+  :dependencies [[prismatic/plumbing "0.5.0"]
                  [prismatic/schema "0.4.4"]
                  [metosin/ring-swagger "0.21.0"]
                  [metosin/ring-swagger-ui "2.1.2"]
@@ -17,7 +17,7 @@
   :profiles {:dev {:plugins [[lein-midje "3.1.3"]]
                    :source-paths ["dev-src" "src"]
                    :dependencies [[org.clojure/clojure "1.7.0"]
-                                  [http-kit "2.1.18"]
+                                  [http-kit "2.1.19"]
                                   [midje "1.7.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
   :aliases {"all" ["with-profile" "dev:dev,1.6"]
