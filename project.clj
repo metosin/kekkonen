57e36f0e1393bb0ce139e2e0fe726f5d2b9b9b95
Tommi Reiman <tommi@iki.fi>
2015-11-02 08:29:46 +0200

    added jsr166y-dependency to get Kekkonen working with Java 1.6

--- project.clj
+++ project.clj
@@ -21,6 +21,8 @@
                    :dependencies [[org.clojure/clojure "1.7.0"]
                                   [criterium "0.4.3"]
                                   [http-kit "2.1.19"]
+                                  ; required when working with Java 1.6
+                                  [org.codehaus.jsr166-mirror/jsr166y "1.7.0"]
                                   [midje "1.8.1"]]}
              :perf {:jvm-opts ^:replace []}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
