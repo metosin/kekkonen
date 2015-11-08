53a89cda29ce441706b0f4e33d69b10765089e08
Tommi Reiman <tommi@iki.fi>
2015-11-08 22:11:40 +0200

    fix tests to work with new ring-middleware-format

--- test/kekkonen/http_test.clj
+++ test/kekkonen/http_test.clj
@@ -1,6 +1,7 @@
 (ns kekkonen.http-test
   (:require [midje.sweet :refer :all]
             [ring.util.http-response :refer [ok]]
+            [ring.util.http-predicates :refer [ok?]]
             [plumbing.core :as p]
             [kekkonen.http :as h]))
 
@@ -16,19 +17,19 @@
 (facts "web-options"
   (let [app (h/http-api {:core {:handlers {:api 'kekkonen.http-test}}})]
 
-    (fact "get"     (app {:uri "/api/get-it",     :request-method :get})     => (ok))
-    (fact "head"    (app {:uri "/api/head-it",    :request-method :head})    => (ok))
-    (fact "patch"   (app {:uri "/api/patch-it",   :request-method :patch})   => (ok))
-    (fact "delete"  (app {:uri "/api/delete-it",  :request-method :delete})  => (ok))
-    (fact "options" (app {:uri "/api/options-it", :request-method :options}) => (ok))
-    (fact "post"    (app {:uri "/api/post-it",    :request-method :post})    => (ok))
-    (fact "put"     (app {:uri "/api/put-it",     :request-method :put})     => (ok))
+    (fact "get"     (app {:uri "/api/get-it",     :request-method :get})     => ok?)
+    (fact "head"    (app {:uri "/api/head-it",    :request-method :head})    => ok?)
+    (fact "patch"   (app {:uri "/api/patch-it",   :request-method :patch})   => ok?)
+    (fact "delete"  (app {:uri "/api/delete-it",  :request-method :delete})  => ok?)
+    (fact "options" (app {:uri "/api/options-it", :request-method :options}) => ok?)
+    (fact "post"    (app {:uri "/api/post-it",    :request-method :post})    => ok?)
+    (fact "put"     (app {:uri "/api/put-it",     :request-method :put})     => ok?)
 
     (fact "any"
-      (app {:uri "/api/any-it", :request-method :get})     => (ok)
-      (app {:uri "/api/any-it", :request-method :head})    => (ok)
-      (app {:uri "/api/any-it", :request-method :patch})   => (ok)
-      (app {:uri "/api/any-it", :request-method :delete})  => (ok)
-      (app {:uri "/api/any-it", :request-method :options}) => (ok)
-      (app {:uri "/api/any-it", :request-method :post})    => (ok)
-      (app {:uri "/api/any-it", :request-method :put})     => (ok))))
+      (app {:uri "/api/any-it", :request-method :get})     => ok?
+      (app {:uri "/api/any-it", :request-method :head})    => ok?
+      (app {:uri "/api/any-it", :request-method :patch})   => ok?
+      (app {:uri "/api/any-it", :request-method :delete})  => ok?
+      (app {:uri "/api/any-it", :request-method :options}) => ok?
+      (app {:uri "/api/any-it", :request-method :post})    => ok?
+      (app {:uri "/api/any-it", :request-method :put})     => ok?)))
