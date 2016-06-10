01b72deb4286a63819d4792b565d0309435f2ba0
Taras Verhun <tarassel@gmail.com>
2016-06-11 00:21:54 +0300

    simple but OK version

--- examples/get-post/src/get_post/core.clj
+++ examples/get-post/src/get_post/core.clj
@@ -3,37 +3,50 @@
             [plumbing.core :refer [defnk]]
             [kekkonen.http :refer :all]
             [kekkonen.cqrs :as cqrs]
-            [kekkonen.core :as k]))
+            [kekkonen.core :as k]
+            [schema.core :as s]))
+
+(s/defschema GetInput
+             {:name s/Str
+              (s/optional-key :description) s/Str})
+
+(s/defn get-handler
+         "Echoes a GetInput"
+         [data :- GetInput]
+         (cqrs/success data))
+
+
+(s/defschema PostInput
+             {:data s/Str})
+
+(s/defn post-handler
+        "Echoes a PostInput"
+        [data :- PostInput]
+        (cqrs/success data))
 
 (defnk ^:get-post get-and-post
        "handles both"
-       {:responses {:default {:schema {:message String}}}}
-       [data1 data2 #_[:data name :- String] request]
+       [get-params post-params request]
        (clojure.pprint/pprint request)
        (if (= (:request-method request) :get)
-         (cqrs/success {:message (str "Hello GET, " name)})
-         (cqrs/success {:message (str "Hello POST, " name)})))
+         (s/with-fn-validation (get-handler get-params))
+         (s/with-fn-validation (post-handler post-params))))
 
 (def app (http-api {:core {:handlers {:api [#'get-and-post]}
                            :type-resolver (k/type-resolver :get :get-post :post)}
                     :ring {:types {:get-post {:methods #{:get :post}
                                          ; :query-params comes from Ring https://github.com/ring-clojure/ring/wiki/Parameters
-                                         :parameters {[:data1] [:request :query-params]
-                                                      [:data2] [:request :body-params]}}}}}))
+                                         :parameters {[:get-params] [:request :query-params]
+                                                      [:post-params] [:request :body-params]}}}}}))
 
 (defonce server (atom nil))
 
 (defn stop-server []
   (when-not (nil? @server)
-    ;; graceful shutdown: wait 100ms for existing requests to be finished
-    ;; :timeout is optional, when no timeout, stop immediately
     (@server :timeout 100)
     (reset! server nil)))
 
 (defn -main [&args]
-  ;; The #' is useful when you want to hot-reload code
-  ;; You may want to take a look: https://github.com/clojure/tools.namespace
-  ;; and http://http-kit.org/migration.html#reload
   (reset! server (server/run-server #'app {:port 3000}))
   (println "server running in port 3000"))
 
