4447c1c0614977f48cf6b82145d4fd4557588b46
Taras Verhun <tarassel@gmail.com>
2016-06-11 12:13:27 +0300

    plus error handler

--- examples/get-post/src/get_post/core.clj
+++ examples/get-post/src/get_post/core.clj
@@ -1,19 +1,18 @@
 (ns get-post.core
   (:require [org.httpkit.server :as server]
             [plumbing.core :refer [defnk]]
-            [kekkonen.http :refer :all]
             [kekkonen.cqrs :as cqrs]
             [kekkonen.core :as k]
             [schema.core :as s]))
 
 (s/defschema GetInput
-             {:name s/Str
+             {:name                         s/Str
               (s/optional-key :description) s/Str})
 
 (s/defn get-handler
-         "Echoes a GetInput"
-         [data :- GetInput]
-         (cqrs/success data))
+        "Echoes a GetInput"
+        [data :- GetInput]
+        (cqrs/success data))
 
 (s/defschema PostInput
              {:data s/Str})
@@ -24,9 +23,8 @@
         (cqrs/success data))
 
 (defnk ^:get-post get-and-post
-       "handles both"
+       "handles both requests"
        [get-params post-params request]
-       #_(clojure.pprint/pprint request)
        (if (= (:request-method request) :get)
          (s/with-fn-validation (get-handler get-params))
          (s/with-fn-validation (post-handler post-params))))
@@ -38,13 +36,19 @@
     (println (str request-method ": " uri))
     ctx))
 
-(def app (http-api {:core {:handlers {:api [#'get-and-post]}
-                           :type-resolver (k/type-resolver :get :get-post :post)
-                           }
-                    :ring {:types {:get-post {:methods #{:get :post}
-                                         ; :query-params comes from Ring https://github.com/ring-clojure/ring/wiki/Parameters
-                                         :parameters {[:get-params] [:request :query-params]
-                                                      [:post-params] [:request :body-params]}}}
+(defn err-handler [ex data req]
+  "logs exception message and return info to client"
+  (clojure.pprint/pprint (.getMessage ex))
+  (cqrs/failure (.getMessage ex)))
+
+(def app (http-api {:core {:handlers      {:api [#'get-and-post]}
+                           :type-resolver (k/type-resolver :get :get-post :post)}
+                    :mw   {:exceptions {#_:default #_err-handler
+                                        :handlers {:schema.core/error err-handler}}}
+                    :ring {:types        {:get-post {:methods    #{:get :post}
+                                                     ; :query-params comes from Ring https://github.com/ring-clojure/ring/wiki/Parameters
+                                                     :parameters {[:get-params]  [:request :query-params]
+                                                                  [:post-params] [:request :body-params]}}}
                            :interceptors [interceptor]}}))
 
 (defonce server (atom nil))
