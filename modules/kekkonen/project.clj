eac313b5ca38917d748703d40fbcebe72721db5c
Miikka Koskinen <miikka@metosin.fi>
2019-11-01 15:23:55 +0200

    Add the :scm info everywhere

--- modules/kekkonen/project.clj
+++ modules/kekkonen/project.clj
@@ -2,6 +2,9 @@
   :description "A lightweight, remote api library for Clojure."
   :url "https://github.com/metosin/kekkonen"
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/metosin/kekkonen"
+        :dir "../.."}
   :plugins [[lein-parent "0.3.4"]]
   :parent-project {:path "../../project.clj"
                    :inherit [:deploy-repositories :managed-dependencies]}
