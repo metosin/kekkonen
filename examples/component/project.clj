a9b44e5670d02666c6a219a6ad79da9b05ad4b36
Miikka Koskinen <miikka@metosin.fi>
2019-11-01 15:18:45 +0200

    Version 0.5.2-SNAPSHOT

--- examples/component/project.clj
+++ examples/component/project.clj
@@ -5,7 +5,7 @@
                  [com.stuartsierra/component "0.3.1"]
                  [reloaded.repl "0.2.2"]
                  [metosin/palikka "0.5.1"]
-                 [metosin/kekkonen "0.5.1"]]
+                 [metosin/kekkonen "0.5.2-SNAPSHOT"]]
   :profiles {:uberjar {:aot [sample.main]
                        :main sample.main
                        :uberjar-name "sample.jar"}})
